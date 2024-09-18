b9306cb1d9a7e72586920c3c4838b3845fb954d3
Renardo-7 <eugenezrg@gmail.com>
2024-09-18 11:14:34 +0200

    first upadate

--- projet math.py
+++ projet math.py
@@ -18,18 +18,29 @@ u=0
 
 tableau_milieu_aquatique_initial = [[] for y in range(taille_y)]
 
-# print(tableau_milieu_aquatique)
 
 for x in range(0,taille_x):
     for y in range(0,taille_y):
         tableau_milieu_aquatique_initial[x].append(0)
 
 
-# for x in range(0,taille_x):
-#     for y in range(0,taille_y):
-#         tableau_milieu_aquatique[x][y] = 0
 
 tableau_milieu_aquatique = tableau_milieu_aquatique_initial
+
+
+def afficherGraphic(harvest):
+
+    fig,ax=plt.subplots()
+    im = ax.imshow(harvest)
+
+    ax.set_xticks(np.arange(0,taille_x))
+    ax.set_yticks(np.arange(0,taille_y))
+
+    plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
+
+    ax.set_title("Test")
+    fig.tight_layout()
+    plt.show()
 
 def afficher (tableau_milieu_aquatique):
     print("\n")
@@ -40,10 +51,15 @@ def afficher (tableau_milieu_aquatique):
 afficher (tableau_milieu_aquatique)
 
 def get(i,j,tableau_milieu_aquatique_n):
-    if(i+1<taille_x and i-1> 0 and j+1 <taille_y and j-1 >0):
-        return tableau_milieu_aquatique_n[i][j]
+    # if x<0 or x>len(tableau_milieu_aquatique_n)-1:
+    #     return 0
+    # if y<0 or y>len(tableau_milieu_aquatique_n)-1:
+    #     return 0
+    # return tableau_milieu_aquatique_n[x][y]
+    if(i+1>taille_x-1 or i-1< 0 or j+1 >taille_y-1 or j-1 <0):
+        return 0
     
-    return 0
+    return tableau_milieu_aquatique_n[i][j]
 
 def C(i,j,tableau_milieu_aquatique_n):
     newC = 0
@@ -63,7 +79,6 @@ def actualiser(tableau_milieu_aquatique):
     for x in range(0,taille_x):
         for y in range(0,taille_y):
             nouveau_tableau[x][y] = C(x,y,tableau_milieu_aquatique)
-    afficher(nouveau_tableau)
     return nouveau_tableau
 
 tableau_milieu_aquatique[4][4] = 100
@@ -72,38 +87,18 @@ tableau_milieu_aquatique[5][4] = 100
 tableau_milieu_aquatique[5][5] = 100
 
 
-
-
+def tourner(n,tableau_milieu_aquatique):
+    for i in range(n):
+        tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique)
+    return tableau_milieu_aquatique
 # tableau_milieu_aquatique[5][5] = C(5,5,tableau_milieu_aquatique)
 
 afficher (tableau_milieu_aquatique)
-for i in range(100):
-    tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique)
-# tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique)
+
+tableau_milieu_aquatique = tourner(100,tableau_milieu_aquatique)
+afficherGraphic(tableau_milieu_aquatique)
 
 
 
-
-# vegetables = ["cucumber", "tomato", "lettuce", "asparagus",
-#  "potato", "wheat", "barley"]
-# farmers = ["Farmer Joe", "Upland Bros.", "Smith Gardening",
-#  "Agrifun", "Organiculture", "BioGoods Ltd.", "Cornylee Corp."]
-
-harvest = tableau_milieu_aquatique
-
-fig,ax=plt.subplots()
-im = ax.imshow(harvest)
-
-ax.set_xticks(np.arange(taille_x))
-ax.set_yticks(np.arange(taille_x))
-
-plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
-
-# for i in range(taille_x):
-#     for j in range(taille_y):
-#         text = ax.text(j,i,round(harvest[i][j],1),ha="center",va="center",color="w")
-#         print("")
-
-ax.set_title("Test")
-fig.tight_layout()
-plt.show()
+# tourner(100,tableau_milieu_aquatique)
+# afficherGraphic(tableau_milieu_aquatique)
