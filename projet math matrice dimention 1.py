14d8092d494546a4d7f57e2245fb9a42c57c3d19
Renardo-7 <eugenezrg@gmail.com>
2024-10-02 11:16:19 +0200

    update

--- projet math matrice dimention 1.py
+++ projet math matrice dimention 1.py
@@ -5,13 +5,11 @@ import matplotlib.pyplot as plt
 D= 0.25
 
 delta_x=1
-delta_y=1
 
 delta_t=1
 
 
 taille_x=10
-taille_y=10
 
 u=0
 
@@ -23,10 +21,10 @@ murHaut = True
 murBas =True
 
 
-n=100
+n=1000
 
 
-tableau_milieu_aquatique_initial = [[] for y in range(taille_y)]
+tableau_milieu_aquatique_initial = []
 
 
 for x in range(0,taille_x):
@@ -38,10 +36,10 @@ for x in range(0,taille_x):
 tableau_milieu_aquatique = tableau_milieu_aquatique_initial
 
 
-def afficherGraphic(harvest):
+def afficherGraphic(tab):
 
     fig,ax=plt.subplots()
-    im = ax.imshow(harvest)
+    im = ax.imshow(tab)
 
     # ax.set_xticks(np.arange(0,taille_x))
     # ax.set_yticks(np.arange(0,taille_y))
@@ -54,36 +52,83 @@ def afficherGraphic(harvest):
 
 def afficher (tableau_milieu_aquatique):
     print("\n")
-    for x in range(0,taille_x):
-        print(tableau_milieu_aquatique[x])
+    # for x in range(0,taille_x):
+    print(tableau_milieu_aquatique)
     print("\n")
 
 
-def get(i,j,tableau_milieu_aquatique_n):
-    # filtre
-    if(i+1>taille_x-1 or i-1< 0 or j+1 >taille_y-1 or j-1 <0):
-        return 0
-    
-    return tableau_milieu_aquatique_n[i][j]
 
 
-def actualiser(tableau_milieu_aquatique):
-    nouveau_tableau = [[] for y in range(taille_y)]
+def actualiser(tableau_milieu_aquatique,tableau_calcul,tableau_constant):
+    # nouveau_tableau = []
+    nouveau_tableau = np.dot(tableau_calcul,tableau_milieu_aquatique)
 
-    # Polution permanente
-    # nouveau_tableau[4][4] = 1
+    nouveau_tableau = np.add(nouveau_tableau,tableau_constant)
 
-    # for x in range(0,taille_x):
-        # for y in range(0,taille_y):
-            # nouveau_tableau[x].append(0)
+    # nouveau_tableau = tableau_milieu_aquatique
 
-    for x in range(1,taille_x-1):
-        for y in range(1,taille_y-1):
-            nouveau_tableau[x][y] = C(x,y,tableau_milieu_aquatique)
+# TODO faire les matrices
+
+
+
     return nouveau_tableau
+
+
+
+def tourner(n,tableau_milieu_aquatique,tableau_calcul,tableau_constant):
+    for i in range(n):
+        tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique,tableau_calcul,tableau_constant)
+    return tableau_milieu_aquatique
+
+
+
+tableau_calcul = [[] for y in range(taille_x)]
+
+for x in range(0,taille_x):
+    for y in range(0,taille_x):
+        if(y-x == -1):
+            tableau_calcul[x].append(D*delta_t/delta_x**2)
+        elif(y-x == 0):
+            tableau_calcul[x].append(1-2*D*delta_t/delta_x**2)
+        elif(y-x == 1):
+            tableau_calcul[x].append(D*delta_t/delta_x**2)
+        else :
+            tableau_calcul[x].append(0)
 
 tableau_milieu_aquatique[4] = 1
 
+tableau_milieu_aquatique=np.matrix(tableau_milieu_aquatique)
+
+tableau_milieu_aquatique = tableau_milieu_aquatique.transpose()
+
+tableau_calcul=np.matrix(tableau_calcul)
+
+tableau_constant = []
+
+for x in range(0,taille_x):
+    # for y in range(0,taille_y):
+    tableau_constant.append(0)
+
+tableau_constant=np.matrix(tableau_constant)
+
+tableau_constant = tableau_constant.transpose()
 
 
-afficherGraphic(tableau_milieu_aquatique)
+# MODIFIER TABLEAU CONSTANT POUR AVOIR DES MURS ET PAS DES FILTRES
+
+
+
+
+afficher(tableau_calcul)
+
+
+afficher(tableau_milieu_aquatique)
+
+
+
+tableau_milieu_aquatique = tourner(n,tableau_milieu_aquatique,tableau_calcul,tableau_constant)
+
+
+afficher(tableau_milieu_aquatique)
+
+# afficherGraphic(tableau_milieu_aquatique)
