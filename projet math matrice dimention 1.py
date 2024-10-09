61016d54e5608b498239d975111f9c0a7aa203ee
Renardo-7 <eugenezrg@gmail.com>
2024-10-09 10:37:49 +0200

    update both

--- projet math matrice dimention 1.py
+++ projet math matrice dimention 1.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-D= 0.10
+D= 0.2
 
 delta_x=1
 delta_y=1
@@ -10,8 +10,8 @@ delta_y=1
 delta_t=1
 
 
-taille_x=10
-taille_y=10
+taille_x=5
+taille_y=5
 
 
 u=0
@@ -24,138 +24,192 @@ murHaut = True
 murBas =True
 
 
-n=4
+n=2
 
+if((2*D)*((delta_t/delta_x**2)+(delta_t/delta_y**2))<=1):
 
-tableau_milieu_aquatique_initial = [[] for y in range(taille_x)]
-for x in range(0,taille_x):
-    for y in range(0,taille_x):
-        tableau_milieu_aquatique_initial[x].append(0)
-
-
-
-tableau_milieu_aquatique = tableau_milieu_aquatique_initial
-
-
-def afficherGraphic(tab):
-
-    fig,ax=plt.subplots()
-    im = ax.imshow(tab)
-
-    plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
-
-    ax.set_title("Propagation d'un poluant")
-    fig.tight_layout()
-    plt.show()
-
-def afficher (tableau_milieu_aquatique):
-    print("\n")
+    tableau_milieu_aquatique_initial = [[] for y in range(taille_x)]
     for x in range(0,taille_x):
-        print(tableau_milieu_aquatique[x])
-    print("\n")
+        for y in range(0,taille_x):
+            tableau_milieu_aquatique_initial[x].append(0)
+
+
+
+    tableau_milieu_aquatique = tableau_milieu_aquatique_initial
+
+
+    def afficherGraphic(tab):
+
+        fig,ax=plt.subplots()
+        im = ax.imshow(tab)
+
+        plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
+
+        ax.set_title("Propagation d'un poluant")
+        fig.tight_layout()
+        plt.show()
+
+    def afficher (tableau_milieu_aquatique):
+        print("\n")
+        for x in range(0,taille_x):
+            print(tableau_milieu_aquatique[x])
+        print("\n")
 
 
 
 
-def actualiser(tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant):
-    nouveau_tableau1 = np.dot(tableau_calcul_x,tableau_milieu_aquatique)
-    nouveau_tableau2 = np.dot(tableau_milieu_aquatique ,tableau_calcul_y)
+    def actualiser(tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant_x,tableau_constant_y):
+        nouveau_tableau1 = np.dot(tableau_calcul_x,tableau_milieu_aquatique)
+        nouveau_tableau2 = np.dot(tableau_milieu_aquatique ,tableau_calcul_y)
 
-    nouveau_tableau = np.add(nouveau_tableau1,nouveau_tableau2)
-    
-    # nouveau_tableau = np.add(nouveau_tableau,nouveau_tableau2)
-    
-    
-    nouveau_tableau = np.add(nouveau_tableau,tableau_constant)
-
-
-    return nouveau_tableau
-
-
-
-def tourner(n,tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant):
-    for i in range(n):
-        tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant)
-    return tableau_milieu_aquatique
-
-
-
-tableau_calcul_x = [[] for y in range(taille_x)]
-
-for x in range(0,taille_x):
-    for y in range(0,taille_x):
-        if(y-x == -1):
-            tableau_calcul_x[x].append((D*delta_t/delta_x**2))
-        elif(y-x == 0):
-            tableau_calcul_x[x].append((1/2-2*D*delta_t/delta_x**2))
-        elif(y-x == 1):
-            tableau_calcul_x[x].append((D*delta_t/delta_x**2))
-        else :
-            tableau_calcul_x[x].append(0)
-
-tableau_calcul_y = [[] for y in range(taille_x)]
-
-for x in range(0,taille_x):
-    for y in range(0,taille_x):
-        if(y-x == -1):
-            tableau_calcul_y[x].append((D*delta_t/delta_y**2))
-        elif(y-x == 0):
-            tableau_calcul_y[x].append((1/2-2*D*delta_t/delta_y**2))
-        elif(y-x == 1):
-            tableau_calcul_y[x].append((D*delta_t/delta_y**2))
-        else :
-            tableau_calcul_y[x].append(0)
-
-
-
+        nouveau_tableau = np.add(nouveau_tableau1,nouveau_tableau2)
         
+        # nouveau_tableau = np.add(nouveau_tableau,nouveau_tableau2)
+        
+        tableau_constant1 = np.dot(tableau_milieu_aquatique ,tableau_constant_x)
+        tableau_constant2 = np.dot(tableau_constant_y ,tableau_milieu_aquatique)
 
-tableau_milieu_aquatique[4][4] = 1
-
-tableau_milieu_aquatique=np.matrix(tableau_milieu_aquatique)
-
-tableau_milieu_aquatique = tableau_milieu_aquatique.transpose()
-
-tableau_calcul_x=np.matrix(tableau_calcul_x)
-tableau_calcul_y=np.matrix(tableau_calcul_y)
+        tableau_constant = np.add(tableau_constant1,tableau_constant2)
 
 
-tableau_constant = [[] for y in range(taille_x)]
-
-for x in range(0,taille_x):
-    for y in range(0,taille_x):
-        tableau_constant[x].append(0)
-
-tableau_constant=np.matrix(tableau_constant)
-
-tableau_constant = tableau_constant.transpose()
+        nouveau_tableau = np.add(nouveau_tableau,tableau_constant)
 
 
-# MODIFIER TABLEAU CONSTANT POUR AVOIR DES MURS ET PAS DES FILTRES
+        return nouveau_tableau
 
 
 
-
-afficher(tableau_calcul_x)
-
-
-afficher(tableau_milieu_aquatique)
-
+    def tourner(n,tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant_x,tableau_constant_y):
+        for i in range(n):
+            tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant_x,tableau_constant_y)
+        return tableau_milieu_aquatique
 
 
-tableau_milieu_aquatique = tourner(n,tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant)
+
+    tableau_calcul_x = [[] for y in range(taille_x)]
+
+    for x in range(0,taille_x):
+        for y in range(0,taille_x):
+            if(y-x == -1):
+                tableau_calcul_x[x].append((D*delta_t/delta_x**2))
+            elif(y-x == 0):
+                tableau_calcul_x[x].append((1/2-2*D*delta_t/delta_x**2))
+            elif(y-x == 1):
+                tableau_calcul_x[x].append((D*delta_t/delta_x**2))
+            else :
+                tableau_calcul_x[x].append(0)
+
+    tableau_calcul_y = [[] for y in range(taille_x)]
+
+    for x in range(0,taille_x):
+        for y in range(0,taille_x):
+            if(y-x == -1):
+                tableau_calcul_y[x].append((D*delta_t/delta_y**2))
+            elif(y-x == 0):
+                tableau_calcul_y[x].append((1/2-2*D*delta_t/delta_y**2))
+            elif(y-x == 1):
+                tableau_calcul_y[x].append((D*delta_t/delta_y**2))
+            else :
+                tableau_calcul_y[x].append(0)
 
 
-afficher(tableau_milieu_aquatique)
-
-afficherGraphic(tableau_milieu_aquatique)
-
-def calcul(tab):
-    total = 0
-    afficher(tab)
+    tableau_constant_x = [[] for y in range(taille_x)]
     for x in range(0,taille_x):
         for y in range(0,taille_y):
-            total += tab[x,y]
-    print("total : "+str(total))
+            if(x== taille_x-1 and y == taille_y-1):
+                if(y-x == -1):
+                    tableau_constant_x[x].append((D*delta_t/delta_x**2)*2)
+                elif(y-x == 0):
+                    tableau_constant_x[x].append((1/2-2*D*delta_t/delta_x**2)*2)
+                elif(y-x == 1):
+                    tableau_constant_x[x].append((D*delta_t/delta_x**2)*2)
+                else :
+                    tableau_constant_x[x].append(0)
+            else :
+                tableau_constant_x[x].append(0)
+
+    print("constant x : ")
+    afficher(tableau_constant_x)
+
+    tableau_constant_y = [[] for y in range(taille_x)]
+    for x in range(0,taille_x):
+        for y in range(0,taille_y):
+            if(x== taille_x-1 and y == taille_y-1):
+                if(y-x == -1):
+                    tableau_constant_y[x].append((D*delta_t/delta_y**2)*2)
+                elif(y-x == 0):
+                    tableau_constant_y[x].append((1/2-2*D*delta_t/delta_y**2)*2)
+                elif(y-x == 1):
+                    tableau_constant_y[x].append((D*delta_t/delta_y**2)*2)
+                else :
+                    tableau_constant_y[x].append(0)
+            else :
+                tableau_constant_y[x].append(0)
+
+
+    tableau_milieu_aquatique[4][4] = 1
+
+    tableau_milieu_aquatique=np.matrix(tableau_milieu_aquatique)
+
+    tableau_milieu_aquatique = tableau_milieu_aquatique.transpose()
+
+    tableau_calcul_x=np.matrix(tableau_calcul_x)
+    tableau_calcul_y=np.matrix(tableau_calcul_y)
+
+    tableau_constant_x=np.matrix(tableau_constant_x)
+    tableau_constant_y=np.matrix(tableau_constant_y)
+
+
+
+
+    # tableau_constant = [[] for y in range(taille_x)]
+
+    # for x in range(0,taille_x):
+    #     for y in range(0,taille_y):
+    #         tableau_constant[x].append(0)
     
-calcul(tableau_milieu_aquatique)
+    # for x in range(0,taille_x):
+    #     for y in range(0,taille_y):
+    #         if(x==0 or x==taille_x-1):
+    #             tableau_constant[x][y]=1
+    #         if(y==0 or y==taille_y-1):
+    #             tableau_constant[x][y]=1
+
+    # afficher(tableau_constant)
+
+
+    # tableau_constant=np.matrix(tableau_constant)
+
+    # tableau_constant = tableau_constant.transpose()
+
+
+    # MODIFIER TABLEAU CONSTANT POUR AVOIR DES MURS ET PAS DES FILTRES
+
+
+
+
+    afficher(tableau_calcul_x)
+
+
+    afficher(tableau_milieu_aquatique)
+
+
+
+    tableau_milieu_aquatique = tourner(n,tableau_milieu_aquatique,tableau_calcul_x,tableau_calcul_y,tableau_constant_x,tableau_constant_y)
+
+
+    afficher(tableau_milieu_aquatique)
+
+    afficherGraphic(tableau_milieu_aquatique)
+
+    def calcul(tab):
+        total = 0
+        afficher(tab)
+        for x in range(0,taille_x):
+            for y in range(0,taille_y):
+                total += tab[x,y]
+        print("total : "+str(total))
+        
+    calcul(tableau_milieu_aquatique)
+else :
+    print("\033[91mpropagation trop elever en x ou en y\033[0m")
--- projet math.py
+++ projet math.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # D = 10**-1
-D= 0.10
+D= 0.2
 # si D*Delta t/delta x ** 2 > 1/4 c'est pas possible les cases polue plus que leur propre concentration
 # si D*Delta t/delta y ** 2 > 1/4 c'est pas possible les cases polue plus que leur propre concentration
 
@@ -13,8 +13,8 @@ delta_y=1
 delta_t=1
 
 
-taille_x=10
-taille_y=10
+taille_x=5
+taille_y=5
 
 u=0
 
@@ -26,120 +26,121 @@ murHaut = True
 murBas =True
 
 
-n=4
+n=1
 
-if((D*delta_t)/(delta_x**2)>1/4):
-    print("propagation trop elever en x")
-if((D*delta_t)/(delta_y**2)>1/4):
-    print("propagation trop elever en y")
+if((2*D)*((delta_t/delta_x**2)+(delta_t/delta_y**2))<=1):
 
 
 
 
-tableau_milieu_aquatique_initial = [[] for y in range(taille_y)]
 
+    tableau_milieu_aquatique_initial = [[] for y in range(taille_y)]
 
-for x in range(0,taille_x):
-    for y in range(0,taille_y):
-        tableau_milieu_aquatique_initial[x].append(0)
-
-
-
-tableau_milieu_aquatique = tableau_milieu_aquatique_initial
-
-
-def afficherGraphic(harvest):
-
-    fig,ax=plt.subplots()
-    im = ax.imshow(harvest)
-
-
-    plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
-
-    ax.set_title("Propagation d'un polluant")
-    fig.tight_layout()
-    plt.show()
-
-def afficher (tableau_milieu_aquatique):
-    print("\n")
-    for x in range(0,taille_x):
-        print(tableau_milieu_aquatique[x])
-    print("\n")
-
-# afficher (tableau_milieu_aquatique)
-
-def get(i,j,tableau_milieu_aquatique_n):
-    # filtre
-    if(i+1>taille_x-1 or i-1< 0 or j+1 >taille_y-1 or j-1 <0):
-        return 0
-    
-    return tableau_milieu_aquatique_n[i][j]
-
-def C(i,j,tableau_milieu_aquatique_n):
-    newC = 0
-    newC= get(i,j,tableau_milieu_aquatique_n)
-
-    droite,gauche,haut,bas = 1,1,1,1
-    if(murBas):
-        if(i+1>=taille_y):
-            bas = 0
-    if(murHaut):
-        if(i<0):
-            haut = 0
-    if(murDroite):
-        if(j+1>=taille_x):
-            droite = 0
-    if(murGauche):
-        if(j<0):
-            gauche = 0
-    newC+= ((D*delta_t)/(delta_x**2))*(get(i+1,j,tableau_milieu_aquatique_n)-droite*get(i,j,tableau_milieu_aquatique_n)-gauche*get(i,j,tableau_milieu_aquatique_n)+get(i-1,j,tableau_milieu_aquatique_n))
-    newC+= ((D*delta_t)/(delta_y**2))*(get(i,j+1,tableau_milieu_aquatique_n)-haut*get(i,j,tableau_milieu_aquatique_n)-bas*get(i,j,tableau_milieu_aquatique_n)+get(i,j-1,tableau_milieu_aquatique_n))
-    newC-= delta_t*u*((get(i+1,j,tableau_milieu_aquatique_n)-get(i-1,j,tableau_milieu_aquatique_n))/(2*delta_x) + (get(i,j+1,tableau_milieu_aquatique_n)-get(i,j-1,tableau_milieu_aquatique_n))/(2*delta_y))
-    return newC
-
-def actualiser(tableau_milieu_aquatique):
-    nouveau_tableau = [[] for y in range(taille_y)]
-
-    # Polution permanente
-    # nouveau_tableau[4][4] = 1
 
     for x in range(0,taille_x):
         for y in range(0,taille_y):
-            nouveau_tableau[x].append(0)
-
-    for x in range(1,taille_x-1):
-        for y in range(1,taille_y-1):
-            nouveau_tableau[x][y] = C(x,y,tableau_milieu_aquatique)
-    return nouveau_tableau
-
-tableau_milieu_aquatique[4][4] = 1
-# tableau_milieu_aquatique[4][5] = 1
-# tableau_milieu_aquatique[5][4] = 1
-# tableau_milieu_aquatique[5][5] = 1
+            tableau_milieu_aquatique_initial[x].append(0)
 
 
-def tourner(n,tableau_milieu_aquatique):
-    for i in range(n):
-        tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique)
-    return tableau_milieu_aquatique
-# tableau_milieu_aquatique[5][5] = C(5,5,tableau_milieu_aquatique)
 
-# afficher (tableau_milieu_aquatique)
-
-tableau_milieu_aquatique = tourner(n,tableau_milieu_aquatique)
-afficherGraphic(tableau_milieu_aquatique)
+    tableau_milieu_aquatique = tableau_milieu_aquatique_initial
 
 
-afficher(tableau_milieu_aquatique)
-# tourner(100,tableau_milieu_aquatique)
-# afficherGraphic(tableau_milieu_aquatique)
+    def afficherGraphic(harvest):
 
-def calcul(tab):
-    total = 0
-    afficher(tab)
-    for x in range(0,taille_x):
-        for y in range(0,taille_y):
-            total += tab[x][y]
-    print("total : "+str(total))
-    
-calcul(tableau_milieu_aquatique)
+        fig,ax=plt.subplots()
+        im = ax.imshow(harvest)
+
+
+        plt.setp(ax.get_xticklabels(),rotation=45,ha="right",rotation_mode="anchor")
+
+        ax.set_title("Propagation d'un polluant")
+        fig.tight_layout()
+        plt.show()
+
+    def afficher (tableau_milieu_aquatique):
+        print("\n")
+        for x in range(0,taille_x):
+            print(tableau_milieu_aquatique[x])
+        print("\n")
+
+    # afficher (tableau_milieu_aquatique)
+
+    def get(i,j,tableau_milieu_aquatique_n):
+        if(i+1>taille_x-1 or i-1< 0 or j+1 >taille_y-1 or j-1 <0):
+            return 0
+        
+        return tableau_milieu_aquatique_n[i][j]
+
+    def C(i,j,tableau_milieu_aquatique_n):
+        newC = 0
+        newC= get(i,j,tableau_milieu_aquatique_n)
+
+        droite,gauche,haut,bas = 1,1,1,1
+        if(murBas):
+            if(i+1>=taille_y):
+                bas = 0
+        if(murHaut):
+            if(i<0):
+                haut = 0
+        if(murDroite):
+            if(j+1>=taille_x):
+                droite = 0
+        if(murGauche):
+            if(j<0):
+                gauche = 0
+        newC+= ((D*delta_t)/(delta_x**2))*(get(i+1,j,tableau_milieu_aquatique_n)-droite*get(i,j,tableau_milieu_aquatique_n)-gauche*get(i,j,tableau_milieu_aquatique_n)+get(i-1,j,tableau_milieu_aquatique_n))
+        newC+= ((D*delta_t)/(delta_y**2))*(get(i,j+1,tableau_milieu_aquatique_n)-haut*get(i,j,tableau_milieu_aquatique_n)-bas*get(i,j,tableau_milieu_aquatique_n)+get(i,j-1,tableau_milieu_aquatique_n))
+        newC-= delta_t*u*((get(i+1,j,tableau_milieu_aquatique_n)-get(i-1,j,tableau_milieu_aquatique_n))/(2*delta_x) + (get(i,j+1,tableau_milieu_aquatique_n)-get(i,j-1,tableau_milieu_aquatique_n))/(2*delta_y))
+        return newC
+
+    def actualiser(tableau_milieu_aquatique):
+        nouveau_tableau = [[] for y in range(taille_y)]
+
+        # Polution permanente
+        # nouveau_tableau[4][4] = 1
+
+        for x in range(0,taille_x):
+            for y in range(0,taille_y):
+                nouveau_tableau[x].append(0)
+
+        for x in range(1,taille_x-1):
+            for y in range(1,taille_y-1):
+                nouveau_tableau[x][y] = C(x,y,tableau_milieu_aquatique)
+        return nouveau_tableau
+
+    tableau_milieu_aquatique[4][4] = 1
+
+    afficher(tableau_milieu_aquatique)
+    # tableau_milieu_aquatique[4][5] = 1
+    # tableau_milieu_aquatique[5][4] = 1
+    # tableau_milieu_aquatique[5][5] = 1
+
+
+    def tourner(n,tableau_milieu_aquatique):
+        for i in range(n):
+            tableau_milieu_aquatique = actualiser(tableau_milieu_aquatique)
+        return tableau_milieu_aquatique
+    # tableau_milieu_aquatique[5][5] = C(5,5,tableau_milieu_aquatique)
+
+    # afficher (tableau_milieu_aquatique)
+
+    tableau_milieu_aquatique = tourner(n,tableau_milieu_aquatique)
+    afficherGraphic(tableau_milieu_aquatique)
+
+
+    afficher(tableau_milieu_aquatique)
+    # tourner(100,tableau_milieu_aquatique)
+    # afficherGraphic(tableau_milieu_aquatique)
+
+    def calcul(tab):
+        total = 0
+        afficher(tab)
+        for x in range(0,taille_x):
+            for y in range(0,taille_y):
+                total += tab[x][y]
+        print("total : "+str(total))
+        
+    calcul(tableau_milieu_aquatique)
+else :
+    print("\033[91mpropagation trop elever en x ou en y\033[0m")
